b41638121df85903507acf4a851b7bf48b7c4511
Zaba505 <cakub6@gmx.com>
2020-06-10 16:27:24 -0400

    test(e2e): add basic coverage for race conditions

--- e2e_test.go
+++ e2e_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"net/http/httptest"
+	"sync"
 	"testing"
 )
 
@@ -46,4 +47,54 @@ func TestE2E(t *testing.T) {
 		t.Fail()
 		return
 	}
+}
+
+func TestConcurrency(t *testing.T) {
+	srv := httptest.NewServer(NewHandler(testHandler))
+	defer srv.Close()
+
+	conn, err := Dial(context.Background(), "ws://"+srv.Listener.Addr().String())
+	if err != nil {
+		t.Errorf("unexpected error when dialing: %s", err)
+		return
+	}
+	defer conn.Close()
+
+	client := NewClient(conn)
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+
+		go func(c Client) {
+			defer wg.Done()
+
+			resp, err := c.Query(context.Background(), &Request{Query: "{ hello { world } }"})
+			if err != nil {
+				t.Errorf("unexpected error when querying: %s", err)
+				return
+			}
+
+			var testResp struct {
+				Hello struct {
+					World string
+				}
+			}
+			err = json.Unmarshal(resp.Data, &testResp)
+			if err != nil {
+				t.Logf("response data: %s", string(resp.Data))
+				t.Errorf("unexpected error when unmarshalling response: %s", err)
+				return
+			}
+
+			if testResp.Hello.World != "this is a test" {
+				t.Logf("expected: %s, but got: %s", "this is a test", testResp.Hello.World)
+				t.Fail()
+				return
+			}
+		}(client)
+	}
+
+	wg.Wait()
 }
